fff6421f0d2359d23e944bccece0e69dc64be2c0
Pavan Kumar <pavanputhra@gmail.com>
2014-01-30 01:35:10 +0530

    Updated DAL

---
+++ Kurukshetra.Hackathon2014.PaymentGateway.Store/IPaymentGatewayRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Kurukshetra.Hackathon2014.PaymentGateway.Store
+{
+    public interface IPaymentGatewayRepository
+    {
+        System.Data.Entity.DbSet<Kurukshetra.Hackathon2014.PaymentGateway.Domain.Account> Accounts { get; set; }
+        System.Data.Entity.DbSet<Kurukshetra.Hackathon2014.PaymentGateway.Domain.AuthenticationChallenge> AuthenticationChallenges { get; set; }
+        System.Data.Entity.DbSet<Kurukshetra.Hackathon2014.PaymentGateway.Domain.Credentials> Credentials { get; set; }
+        System.Data.Entity.DbSet<Kurukshetra.Hackathon2014.PaymentGateway.Domain.Merchant> Merchants { get; set; }
+        System.Data.Entity.DbSet<Kurukshetra.Hackathon2014.PaymentGateway.Domain.PaymentChallenge> PaymentChallenges { get; set; }
+        System.Data.Entity.DbSet<Kurukshetra.Hackathon2014.PaymentGateway.Domain.Person> Persons { get; set; }
+        System.Data.Entity.DbSet<Kurukshetra.Hackathon2014.PaymentGateway.Domain.AccountTransaction> Transactions { get; set; }
+    }
+}
