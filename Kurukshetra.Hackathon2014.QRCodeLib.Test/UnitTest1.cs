91e5f3627057b461921d22388d06c81d958dd769
Sharath Kumar <sharath.reddy207@gmail.com>
2014-01-29 23:24:34 +0530

    Adding ZXing Library

---
+++ Kurukshetra.Hackathon2014.QRCodeLib.Test/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using ZXing.Common;
+
+namespace Kurukshetra.Hackathon2014.QRCodeLib.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            QRConverter converter = new QRConverter();
+            BitMatrix imgMatrix = converter.getQR("Hello World!");
+
+            using (Bitmap bm = new Bitmap(350,350))
+            {
+                    for (int i = 0; i < imgMatrix.Height; i++)
+                    {
+                        for (int j = 0; j < imgMatrix.Width; j++)
+                        {
+                            if (imgMatrix[j, i])
+                                bm.SetPixel(j, i, Color.Black);
+                            else
+                                bm.SetPixel(j, i, Color.White);
+                        }
+                    }
+                    bm.Save("text.png");
+            }
+            
+        }
+    }
+}
---
+++ Kurukshetra.Hackathon2014.QRCodeLib/QRConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZXing.QrCode;
+using ZXing.Common;
+using ZXing.QrCode.Internal;//bytematrix
+using System.Collections;
+
+
+namespace Kurukshetra.Hackathon2014.QRCodeLib
+{
+    public class QRConverter
+    {
+        public BitMatrix getQR(string message)
+        {
+            QRCodeWriter qrCode = new QRCodeWriter();
+            BitMatrix imgBitmap = qrCode.encode(message, ZXing.BarcodeFormat.QR_CODE, 350, 350);
+            return imgBitmap;
+        }
+    }
+}
