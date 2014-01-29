945352e7eacb1a38a4400f426efaa15f89c6db5f
Pavan Kumar <pavanputhra@gmail.com>
2014-01-29 15:35:42 +0530

    PaymentGatewayDbContext Modified
    Renamed file. Added constructors. Added 2 more entities.

--- Kurukshetra.Hackathon2014.PaymentGateway.Store/PaymentGatewayDbContext.cs
+++ Kurukshetra.Hackathon2014.PaymentGateway.Store/PaymentGatewayDbContext.cs
@@ -8,18 +8,31 @@ using Kurukshetra.Hackathon2014.PaymentGateway.Domain;
 
 namespace Kurukshetra.Hackathon2014.PaymentGateway.Store
 {
-    public class PaymentGatewayDbContest : DbContext
+    public class PaymentGatewayDbContext : DbContext
     {
+        public PaymentGatewayDbContext()
+            : base("DefaultConnection")
+        { }
+
+        public PaymentGatewayDbContext(string connectionName) :
+            base(connectionName)
+        { }
+
         public DbSet<Person> Persons { get; set; }
         public DbSet<Account> Accounts { get; set; }
         public DbSet<AccountTransaction> Transactions { get; set; }
         public DbSet<Credentials> Credentials { get; set; }
+        public DbSet<AuthenticationChallenge> AuthenticationChallenges { get; set; }
+        public DbSet<PaymentChallenge> PaymentChallenges { get; set; }
+
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Credentials>().HasKey(t => t.UserName);
+            modelBuilder.Entity<AuthenticationChallenge>().HasKey(t => new { t.UserName, t.EpochTime });
+            modelBuilder.Entity<PaymentChallenge>().HasKey(t => new { t.MerchantId, t.OrderId, t.EpochTime });
         }
     }
 }
