59bc5c96300da4d3a4775564f43983f540f5302c
Pavan Kumar <pavanputhra@gmail.com>
2014-01-29 14:49:27 +0530

    Bare bone of Payment Gatway

--- Kurukshetra.Hackathon2014.PaymentGateway.Web/App_Start/FilterConfig.cs
+++ Kurukshetra.Hackathon2014.PaymentGateway.Web/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace Kurukshetra.Hackathon2014.PaymentGateway.Web
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
---
+++ Kurukshetra.Hackathon2014.PaymentGateway.Web/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kurukshetra.Hackathon2014.PaymentGateway.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult RegisterMerchant()
+        {
+            return View();
+        }
+
+    }
+}
